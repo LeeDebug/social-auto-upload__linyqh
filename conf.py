d46b89a24689fb2f943f80437e0624054bba5471
Edan's Win11 <961150665@qq.com>
2024-12-30 20:27:47 +0800

    fix: 增加注释允许在根目录调用

--- conf.py
+++ conf.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.resolve()
+print("BASE_DIR: ", BASE_DIR)
+# PARENT_DIR = Path(__file__).parent.resolve().parent.resolve()
+# print("PARENT_DIR: ", PARENT_DIR)
 XHS_SERVER = "http://127.0.0.1:11901"
 LOCAL_CHROME_PATH = None
 
--- examples/upload_video_to_kuaishou.py
+++ examples/upload_video_to_kuaishou.py
@@ -1,6 +1,14 @@
 import asyncio
 from pathlib import Path
 
+# ! 如果要在根目录调用该文件，需要添加以下代码
+import sys
+# 获取当前文件的父目录（即 'src' 目录）的父目录（即项目根目录）
+project_root = Path(__file__).parent.parent.resolve()
+# 将项目根目录添加到 Python 的模块搜索路径中
+if str(project_root) not in sys.path:
+    sys.path.append(str(project_root))
+
 from conf import BASE_DIR
 from uploader.ks_uploader.main import ks_setup, KSVideo
 from utils.files_times import generate_schedule_time_next_day, get_title_and_hashtags
