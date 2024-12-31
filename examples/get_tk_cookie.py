0d5a9c601d24c2e87a0af23798cb3a168b88805d
Edan's Win11 <961150665@qq.com>
2024-12-31 13:19:52 +0800

    fix: 完善 tiktok 脚本

--- examples/get_tk_cookie.py
+++ examples/get_tk_cookie.py
@@ -1,9 +1,19 @@
 import asyncio
 from pathlib import Path
 
+# ! 如果要在根目录调用该文件，需要添加以下代码
+import sys
+# 获取当前文件的父目录（即 'src' 目录）的父目录（即项目根目录）
+project_root = Path(__file__).parent.parent.resolve()
+# 将项目根目录添加到 Python 的模块搜索路径中
+if str(project_root) not in sys.path:
+    sys.path.append(str(project_root))
+
 from conf import BASE_DIR
 from uploader.tk_uploader.main_chrome import tiktok_setup
 
 if __name__ == '__main__':
+    print("[get_tk_cookie.py] BASE_DIR: ", BASE_DIR)
     account_file = Path(BASE_DIR / "cookies" / "tk_uploader" / "account.json")
+    print("[get_tk_cookie.py] account_file: ", account_file)
     cookie_setup = asyncio.run(tiktok_setup(str(account_file), handle=True))
