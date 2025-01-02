5721507e41ae7427ce3e6066fce1858bcb6784bd
Edan's Win11 <961150665@qq.com>
2025-01-02 08:22:43 +0800

    feat: 增加 微博 发布流程（尚未完成）

--- examples/get_weibo_cookie.py
+++ examples/get_weibo_cookie.py
@@ -1,1 +1,19 @@
-# https://passport.weibo.com/sso/signin
+import asyncio
+from pathlib import Path
+
+# ! 如果要在根目录调用该文件，需要添加以下代码
+import sys
+# 获取当前文件的父目录（即 'src' 目录）的父目录（即项目根目录）
+project_root = Path(__file__).parent.parent.resolve()
+# 将项目根目录添加到 Python 的模块搜索路径中
+if str(project_root) not in sys.path:
+    sys.path.append(str(project_root))
+
+from conf import BASE_DIR
+from uploader.weibo_uploader.main import weibo_setup
+
+if __name__ == '__main__':
+    print("[get_weibo_cookie.py] BASE_DIR: ", BASE_DIR)
+    account_file = Path(BASE_DIR / "cookies" / "weibo_uploader" / "account.json")
+    print("[get_weibo_cookie.py] account_file: ", account_file)
+    cookie_setup = asyncio.run(weibo_setup(str(account_file), handle=True))
---
+++ examples/upload_video_to_weibo.py
@@ -0,0 +1,37 @@
+import asyncio
+from pathlib import Path
+
+# ! 如果要在根目录调用该文件，需要添加以下代码
+import sys
+# 获取当前文件的父目录（即 'src' 目录）的父目录（即项目根目录）
+project_root = Path(__file__).parent.parent.resolve()
+# 将项目根目录添加到 Python 的模块搜索路径中
+if str(project_root) not in sys.path:
+    sys.path.append(str(project_root))
+
+from conf import BASE_DIR
+from uploader.weibo_uploader.main import weibo_setup, WeiBoVideo
+from utils.files_times import generate_schedule_time_next_day, get_title_and_hashtags
+
+
+if __name__ == '__main__':
+    filepath = Path(BASE_DIR) / "videos"
+    account_file = Path(BASE_DIR / "cookies" / "weibo_uploader" / "account.json")
+    # 获取视频目录
+    folder_path = Path(filepath)
+    # 获取文件夹中的所有文件
+    files = list(folder_path.glob("*.mp4"))
+    file_num = len(files)
+    publish_datetimes = generate_schedule_time_next_day(file_num, 1, daily_times=[6])
+    cookie_setup = asyncio.run(weibo_setup(account_file, handle=False))
+    for index, file in enumerate(files):
+        print("=======================")
+        print("index: ", index)
+        print("file: ", file)
+        title, tags = get_title_and_hashtags(str(file))
+        # 打印视频文件名、标题和 hashtag
+        print(f"视频文件名：{file}")
+        print(f"标题：{title}")
+        print(f"Hashtag：{tags}")
+        app = WeiBoVideo(title, file, tags, publish_datetimes[index], account_file)
+        asyncio.run(app.main(), debug=False)
